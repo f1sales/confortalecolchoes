1c617b68e5e150717c49ad1986e80da9f15c95e3
Thiago Youssef <thiagodyoussef@gmail.com>
2022-02-15 11:55:01 -0300

    Add product filter hook

--- lib/confortalecolchoes.rb
+++ lib/confortalecolchoes.rb
@@ -7,5 +7,21 @@ module Confortalecolchoes
   class Error < StandardError; end
 
   class F1SalesCustom::Hooks::Lead
+    def self.switch_source(lead)
+      source_name = lead.source ? lead.source.name : ''
+      product_name = lead.product.name.downcase
+
+      if product_name.include?('santo andré')
+        "#{source_name} - Santo André"
+      elsif product_name.include?('interlagos')
+        "#{source_name} - Interlagos"
+      elsif product_name.include?('jabaquara')
+        "#{source_name} - Jabaquara"
+      elsif product_name.include?('jurubatuba')
+        "#{source_name} - Jurubatuba"
+      else
+        lead.source.name
+      end
+    end
   end
 end
--- spec/hooks_spec.rb
+++ spec/hooks_spec.rb
@@ -2,4 +2,68 @@ require 'ostruct'
 require 'byebug'
 
 RSpec.describe F1SalesCustom::Hooks::Lead do
+  let(:customer) do
+    customer = OpenStruct.new
+    customer.name = 'Marcio'
+    customer.phone = '1198788899'
+    customer.email = 'marcio@f1sales.com.br'
+
+    customer
+  end
+
+  let(:source) do
+    source = OpenStruct.new
+    source.name = 'Facebook - Confortale Colchões'
+
+    source
+  end
+
+  let(:product) do
+    product = OpenStruct.new
+    product.name = product_name
+
+    product
+  end
+
+  let(:lead) do
+    lead = OpenStruct.new
+    lead.source = source
+    lead.customer = customer
+    lead.product = product
+
+    lead
+  end
+
+
+  context 'when product contains "Jabaquara"' do
+    let(:product_name) { '[CV] [Jabaquara]' }
+
+    it 'returns source name' do
+      expect(described_class.switch_source(lead)).to eq('Facebook - Confortale Colchões - Jabaquara')
+    end
+  end
+
+  context 'when product contains "Santo André"' do
+    let(:product_name) { '[CV] Santo André' }
+
+    it 'returns source name' do
+      expect(described_class.switch_source(lead)).to eq('Facebook - Confortale Colchões - Santo André')
+    end
+  end
+
+  context 'when product contains "Interlagos"' do
+    let(:product_name) { '[CV] Interlagos' }
+
+    it 'returns source name' do
+      expect(described_class.switch_source(lead)).to eq('Facebook - Confortale Colchões - Interlagos')
+    end
+  end
+
+  context 'when product contains "Jurubatuba"' do
+    let(:product_name) { '[CV] Jurubatuba' }
+
+    it 'returns source name' do
+      expect(described_class.switch_source(lead)).to eq('Facebook - Confortale Colchões - Jurubatuba')
+    end
+  end
 end
