422bd5dbeceaa84460240ea6ba1344fcfc9665a9
Thiago Youssef <thiagodyoussef@gmail.com>
2022-02-15 11:10:01 -0300

    Init gem

---
+++ lib/confortalecolchoes.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "confortalecolchoes/version"
+require "f1sales_custom/hooks"
+
+module Confortalecolchoes
+  class Error < StandardError; end
+
+  class F1SalesCustom::Hooks::Lead
+  end
+end
